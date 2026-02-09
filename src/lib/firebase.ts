bf1df3058a9a85a689d46a0201f51729c511bb26
ibrahim ezzine <ibrahimezzine09@gmail.com>
2026-02-09 14:21:23 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAFJKhinY2JrJCK-fMc2xQ9PKdBPKri4DQ",
+  authDomain: "studio-5067807929-254e6.firebaseapp.com",
+  projectId: "studio-5067807929-254e6",
+  storageBucket: "studio-5067807929-254e6.appspot.com",
+  messagingSenderId: "745753571996",
+  appId: "1:745753571996:web:4b3220fa9705e3b5a33359"
 };
 
 // Initialize Firebase
